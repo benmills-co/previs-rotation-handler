0699f02f58e33991e358f71e1d21ec179c50bae9
Ben Mills <31953477+benmills-co@users.noreply.github.com>
2020-07-27 00:30:51 +0100

    File Export Implemented
    Correctly runs maths on the contensts and exports to an automatically named file, which includes the export format.

--- app.py
+++ app.py
@@ -7,28 +7,34 @@ availableExportFormats = ['Capture', 'MA3D']
 # Chosen export format
 exportFormat = ['Capture']
 
-file = 'Jess-TheVic.txt'
-iDataFile = open(file)
+# iData = Instrument Data
+filename = 'RotationTest.txt'
+iDataFile = open(filename)
 iData = csv.DictReader(iDataFile, dialect='excel-tab')
-newIData = []
+newIData = list(iData)
+
 
 # Main Function
 def runExport():
     if exportFormat == ['Capture']:
         print('Chosen export format:', ''.join(exportFormat), '\n')
-        for row in iData:
+        for row in newIData:
             # Make data numeric
+            global X,Y,Z
             X = float(row['X Rotation'])
             Y = float(row['Y Rotation'])
             Z = float(row['Z Rotation'])
             print(row['Instrument Type'], '/ U:', row['Unit Number'], 'on the', row['Position'], '\n- WAS at rotation:      ', X, '/', Y, '/', Z)
 
             # Data Manipulation
-            X = X+90
-            Y = Y+90
-            Z = Z+90
+            X = X+45
+            Y = Y+45
+            Z = Z+45
             X,Y,Z = X,Z,Y
 
+            row['X Rotation'] = X
+            row['Y Rotation'] = Y
+            row['Z Rotation'] = Z
             print('- IS NOW at rotation:   ', X, '/', Y, '/', Z, '\n')
 
     elif exportFormat == ['MA3D']:
@@ -39,9 +45,17 @@ def runExport():
 
 runExport()
 
-splitFile = file.split('.', 1)[0]
-fileSaveName = (''.join(splitFile), ' - for ', ''.join(exportFormat), '.txt')
-print(''.join(fileSaveName))
+# File Export
+splitFile = filename.split('.', 1)[0]
+saveFilename = (''.join(splitFile), ' - for ', ''.join(exportFormat), '.txt')
+print('Exported to: ', ''.join(saveFilename))
 
+
+with open(''.join(saveFilename), 'w', newline='') as csvfile:
+    fieldnames = newIData[1]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel-tab')
+
+    writer.writeheader()
+    writer.writerows(newIData)
 
 iDataFile.close()
